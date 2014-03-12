7c4fcfd50328bb102137724f07f3ab4c63d2ba9d
Wenna <whayudini@emiva.net>
2014-03-12 16:52:21 -0400

    Use Methods with Parameters in Java

--- Wenna/src/apples.java
+++ Wenna/src/apples.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 class apples{
-	public static void main(String [] args){
+	public static void main(String[] args) {
 		
-			salmon salmonObject = new salmon();
-			salmonObject.simpleMessage();
-		
+			Scanner input = new Scanner(System.in);
+			salmon salmonObject = new salmon ();
+			
+			System.out.println("Enter your name here");
+			String name = input.nextLine();
+			
+			salmonObject.simpleMessage(name);
+					
 	}
 }
 
--- Wenna/src/salmon.java
+++ Wenna/src/salmon.java
@@ -1,6 +1,6 @@
 
 public class salmon {
-	public void simpleMessage(){
-		System.out.println("This is another class");
+	public void simpleMessage(String name){
+		System.out.println("Hello " + name);
 	}
 }
