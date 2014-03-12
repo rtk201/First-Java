4d9d6d9541ad81358b07048dc9d9da2980643951
Wenna <whayudini@emiva.net>
2014-03-12 16:23:32 -0400

    Using multiple classes in Java

--- Wenna/src/apples.java
+++ Wenna/src/apples.java
@@ -1,12 +1,9 @@
 class apples{
-	public static void main(String args[]){
-		int counter = 0;
-			
-		while (counter < 10){
-			System.out.println(counter);
-			counter++;
-	
-		}
+	public static void main(String [] args){
+		
+			salmon salmonObject = new salmon();
+			salmonObject.simpleMessage();
+		
 	}
 }
 
