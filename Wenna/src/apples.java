c77bdeb811ca9461a894b40e9519318cdd8501df
Wenna <whayudini@emiva.net>
2014-03-17 15:46:30 -0400

    Tutorial in Java (many instaces)

--- Wenna/src/apples.java
+++ Wenna/src/apples.java
@@ -2,15 +2,12 @@ import java.util.Scanner;
 
 class apples{
 	public static void main(String[] args) {
-		
-			Scanner input = new Scanner(System.in);
+			Scanner kinput = new Scanner(System.in);
 			salmon salmonObject = new salmon ();
-			
-			System.out.println("Enter your name here");
-			String name = input.nextLine();
-			
-			salmonObject.simpleMessage(name);
-					
+			System.out.println("Enter name of first gf here: ");
+			String temp = input.nextLine();
+			salmonObject.setName(temp);
+			salmonObject.saying();		
 	}
 }
 
